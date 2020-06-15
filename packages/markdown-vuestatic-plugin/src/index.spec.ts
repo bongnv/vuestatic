07b3738b6ca1d7aed06ea1d636d37dcc57676bf0
Bong Nguyen <van.bong@grabtaxi.com>
2020-06-15 09:42:46 +0800

    test: fix snapshot

--- packages/markdown-vuestatic-plugin/src/index.spec.ts
+++ packages/markdown-vuestatic-plugin/src/index.spec.ts
@@ -5,32 +5,32 @@ test("MarkdownVueStaticPlugin should create build config", async () => {
   const plugin = new MarkdownVueStaticPlugin();
   expect(plugin.apply).toBeTruthy();
 
-  const e = new Execution({});
+  const e = new Execution({
+    baseDir: "/",
+  });
   plugin.apply(e);
   await e.commands.for("build").promise(e);
   await e.steps.for("config").promise(e);
-  expect(e.config.serverWebpackConfig.toConfig().module?.rules).toHaveLength(3);
+  expect(e.config.serverWebpackConfig.toConfig().module?.rules).toHaveLength(2);
   expect(e.config.serverWebpackConfig.toConfig().resolve?.alias).toHaveProperty(
     "@content",
   );
-  expect(e.config.serverWebpackConfig.toConfig().resolve?.alias).toHaveProperty(
-    "@vuestatic/static-props",
-  );
+  expect(e.config.serverWebpackConfig.toConfig()).toMatchSnapshot();
 });
 
 test("MarkdownVueStaticPlugin should create dev config", async () => {
   const plugin = new MarkdownVueStaticPlugin();
   expect(plugin.apply).toBeTruthy();
 
-  const e = new Execution({});
+  const e = new Execution({
+    baseDir: "/",
+  });
   plugin.apply(e);
   await e.commands.for("dev").promise(e);
   await e.steps.for("config").promise(e);
-  expect(e.config.serverWebpackConfig.toConfig().module?.rules).toHaveLength(3);
+  expect(e.config.serverWebpackConfig.toConfig().module?.rules).toHaveLength(2);
   expect(e.config.serverWebpackConfig.toConfig().resolve?.alias).toHaveProperty(
     "@content",
   );
-  expect(e.config.serverWebpackConfig.toConfig().resolve?.alias).toHaveProperty(
-    "@vuestatic/static-props",
-  );
+  expect(e.config.serverWebpackConfig.toConfig()).toMatchSnapshot();
 });
